42e12f9678ae690fe3d9b189c91d334944e2bc0a
cmarks-moffitt-testing <112427804+cmarks-moffitt-testing@users.noreply.github.com>
2022-08-29 15:29:58 -0400

    Change Message Text

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ func main() {
 
 		f := fib()
 
-		res := &response{Message: "Hello World"}
+		res := &response{Message: "Hello World From Cmarks"}
 
 		for _, e := range os.Environ() {
 			pair := strings.Split(e, "=")
